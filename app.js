6b19bca784674caa4dde80ebf3166e982e73c4ea
Begli <velikijbegli@gmail.com>
2026-06-07 17:12:35 +0500

    implemented cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { syncDatabase } = require('./models');
 const postRoutes = require('./routes/posts');
 const authorRoutes = require('./routes/authors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/posts', postRoutes);
